78274470b5f770f28c85f6a5b031dc798ca9c36d
gangireddynandini <123799756+gangireddynandini@users.noreply.github.com>
2023-05-16 18:14:30 +0530

    Add files via upload

---
+++ constructor.js
@@ -0,0 +1,8 @@
+// alert('hloo world')
+function Boy(age, sub, marks) {
+    this.age = age;
+    this.marks = marks;
+    this.sun = sub;
+}
+var asd = new Boy(23, "science", 78);
+console.log(asd.age)
